293935b62ad493b5e6a2ca9f650971359f229614
Sierra <sierra.clarke@brooklynsteamcenter.org>
2019-05-20 14:40:56 -0400

    Initial Commit

---
+++ Two Buttons/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Two Buttons
+//
+//  Created by Genesis on 5/20/19.
+//  Copyright © 2019 Genesis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Two ButtonsTests/Two_ButtonsTests.swift
@@ -0,0 +1,34 @@
+//
+//  Two_ButtonsTests.swift
+//  Two ButtonsTests
+//
+//  Created by Genesis on 5/20/19.
+//  Copyright © 2019 Genesis. All rights reserved.
+//
+
+import XCTest
+@testable import Two_Buttons
+
+class Two_ButtonsTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
